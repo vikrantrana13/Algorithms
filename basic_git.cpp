924656d4b5ab53bbe3834811fec639a48cc29be0
Vikrant Rana <ranavikrant0012@gmail.com>
2025-04-02 13:44:44 +0530

    add new file

---
+++ basic_git.cpp
@@ -0,0 +1,11 @@
+// 1.git clone /link from the git hub/(it add the gt hub file form git hub to local machine)
+
+// 2.git status (it show the status of github)
+
+// 3.git add /file.name/(it add the file name)
+
+// 4.git commit -m"some message" (it coomi the changes in the git hub)
+
+// unless we run this command we dont see the chnages in the git hub
+
+// 5.git hub push origin main
